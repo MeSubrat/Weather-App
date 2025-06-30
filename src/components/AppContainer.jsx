af5a6365e3c8f6866266a873960ef7dafa74f648
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2025-06-30 12:37:42 +0530

    Fix AppContainer filename and import

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './AppContainer.css'
 import CurrentWeatherDetails from './CurrentWeatherDetails';
 function AppContainer() {
